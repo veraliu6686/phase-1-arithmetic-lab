0acee72f0a29ffed2d0399924da5d46bf10bafdb
lianzi liu <lianziliu.sugar@gmail.com>
2022-10-10 17:00:21 -0400

    commit tree 0322d2df792819de27653203a7aba2de3ad11213

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+console.log (multiply) ;
+
+const random = Math.floor(Math.random() * 10) + 1;
+console.log (random);
+
+let num3 = 14;
+let num4 = 10;
+const mod = num3 % num4;
+console.log(mod);
+
+let max = Math.max (5,10,15,20);
+console.log (max);
